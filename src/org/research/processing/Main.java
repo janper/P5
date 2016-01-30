1ba8b53e80450536633df1a62cab77c9781cbbb6
Jan Pernecky <Jan Pernecky>
2016-01-30 23:03:03 +0100

    Initial commit

---
+++ src/org/research/processing/Main.java
@@ -0,0 +1,12 @@
+package org.research.processing;
+
+import processing.core.PApplet;
+
+/**
+ * Created by Jan on 30.01.2016.
+ */
+public class Main {
+    public static void main(String args[]) {
+        PApplet.main(new String[] {"org.research.processing.ProcessingSketch"});
+    }
+}
